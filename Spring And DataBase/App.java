d16bb3af2a60c97fd13ec4104cd4687afc1c7ac7
warkehi <101081682+warkehi@users.noreply.github.com>
2022-07-11 18:03:42 +0530

    Add files via upload

---
+++ Spring And DataBase/App.java
@@ -0,0 +1,28 @@
+package com.cdac.app;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.cdac.component.CarPart;
+import com.cdac.component.CarPartsInventory;
+
+public class App {
+
+	public static void main(String[] args) {
+		//Loading Spring/IOC Container
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("my-spring-config.xml");
+		
+		CarPartsInventory inv = (CarPartsInventory) ctx.getBean("carParts2");
+		
+		//model/entity classes are not instantiated using Spring
+		CarPart cp = new CarPart();
+		cp.setPartName("Seat Cover");
+		cp.setCarModel("Maruti 800");
+		cp.setPrice(500);
+		cp.setQuantity(75);
+		
+		inv.addNewPart(cp);
+		
+	}
+
+}
